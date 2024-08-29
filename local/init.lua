4213e04597f09170e986af769223d1433364a3fe
Sarah Evermore <sarah.evermore.02@gmail.com>
2024-08-29 11:56:33 +0200

    update local file for future convenience :3

--- local/init.lua
+++ local/init.lua
@@ -1,4 +1,9 @@
 local config = {}
 -- add local configuration here!
 
+-- useful settings I might want
+--
+-- set shell manually
+-- config.default_prog = { "/Users/evermore/.cargo/bin/nu" }
+
 return config
